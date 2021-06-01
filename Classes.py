dd5d916eecc49cafda6b2f5ceed13ea37b55c6d3
0980991 <0980991@hr.nl>
2021-06-01 23:24:20 +0200

    stuff

--- Classes.py
+++ Classes.py
@@ -1,12 +1,35 @@
 import json
+import random
 class GUI:
      def __init__(self):
         print("Password Generator 6900\n" + (15 * "_"))
+        print("What would you like to do today?\n" + (15 * "_") + "\n1.Open password manager\n2.Generate new password\n3. Quit")
+        choice = input()
+        
+        while choice not in range(1-4):
+            if choice == "1":
+                PasswordManager()
+                break
+            if choice == "2":
+                PasswordGenerator()
+                break
 
-class PasswordGenerator:
-    def __init__(): 
-        self.userData = open("userData.json")
+
+
+class PasswordManager:
+    def __init__(self): 
+        self.userData = json.load(open("userData.json"))
+        for username in self.userData:
+            print(type(self.username))
+            print(type(self.userData))
+
                                          
-
+class PasswordGenerator:
+    def Generate(self):
+        password = ""
+        length = input("How many characters would you like your password to be?")
+        for i in length:
+            password += ord(random.randint(x=97, y=122))
+        print(password)
 
                 
--- Generator.py
+++ Generator.py
@@ -1,4 +1,3 @@
+import Classes
 
- class GUI:
-     def __init__(self):
-         self.userData = open("userData.json")
+generator = Classes.GUI()
